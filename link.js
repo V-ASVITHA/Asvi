982191d49c82d5ecdde19c73cdb54d1051daad51
V-ASVITHA <133028554+V-ASVITHA@users.noreply.github.com>
2023-05-09 20:28:34 +0530

    Add files via upload

---
+++ link.js
@@ -0,0 +1,9 @@
+function clicked()
+{
+    alert('clicked');
+}
+switch1.addEventlistener('mouseover',function());
+<div class="icons">
+                    <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"><img src="https://1000logos.net/wp-content/uploads/2021/05/Gmail-logo.png" width="200" height="100" alt="G-Mail"></a><br><br><br><br><br>
+                    <a href="#"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="100" height="100" alt="GitHub"></a>
+                </div>
